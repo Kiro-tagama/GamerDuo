2399445c286e4ab5db17a7f43e7570c9e156a7a9
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2023-06-15 17:08:35 -0300

    add components (fakeProfile)

---
+++ src/api/fakeProfiles.tsx
@@ -0,0 +1,67 @@
+export const mainProfile=[
+    {
+    "id": 1,
+    "email": "perfil1@example.com",
+    "senha": "senha123",
+    "nome": "Perfil 1"
+    },
+    {
+    "id": 1,
+    "email": "perfil1@example.com",
+    "senha": "senha123",
+    "nome": "Perfil 1"
+    }
+]
+
+export const perfis= [
+    {
+      "id": 1,
+      "nome": "Perfil 1",
+      "img": "https://exemplo.com/img1.jpg"
+    },
+    {
+      "id": 2,
+      "nome": "Perfil 2",
+      "img": "https://exemplo.com/img2.jpg"
+    },
+    {
+      "id": 3,
+      "nome": "Perfil 3",
+      "img": "https://exemplo.com/img3.jpg"
+    },
+    {
+      "id": 4,
+      "nome": "Perfil 4",
+      "img": "https://exemplo.com/img4.jpg"
+    },
+    {
+      "id": 5,
+      "nome": "Perfil 5",
+      "img": "https://exemplo.com/img5.jpg"
+    },
+    {
+      "id": 6,
+      "nome": "Perfil 6",
+      "img": "https://exemplo.com/img6.jpg"
+    },
+    {
+      "id": 7,
+      "nome": "Perfil 7",
+      "img": "https://exemplo.com/img7.jpg"
+    },
+    {
+      "id": 8,
+      "nome": "Perfil 8",
+      "img": "https://exemplo.com/img8.jpg"
+    },
+    {
+      "id": 9,
+      "nome": "Perfil 9",
+      "img": "https://exemplo.com/img9.jpg"
+    },
+    {
+      "id": 10,
+      "nome": "Perfil 10",
+      "img": "https://exemplo.com/img10.jpg"
+    }
+]
--- src/screen/Login.tsx
+++ src/screen/Login.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Lógica de autenticação
+    console.log('Login realizado com sucesso!');
+  };
+
+  const handleSignUp = () => {
+    // Navegar para a tela de cadastro
+    console.log('Navegar para a tela de cadastro');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Bem-vindo</Text>
+      <Text style={{ fontSize: 18, marginBottom: 20 }}>Login</Text>
+      <TextInput
+        style={{ height: 40, width: '80%', borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        placeholder="Email"
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        style={{ height: 40, width: '80%', borderColor: 'gray', borderWidth: 1, marginBottom: 20 }}
+        placeholder="Senha"
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+        value={password}
+      />
+      <TouchableOpacity
+        style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5, marginBottom: 10 }}
+        onPress={handleLogin}
+      >
+        <Text style={{ color: 'white', textAlign: 'center', fontSize: 16 }}>Logar</Text>
+      </TouchableOpacity>
+      <Text style={{ marginTop: 20 }}>Não tenho conta</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: 'gray', padding: 10, borderRadius: 5 }}
+        onPress={handleSignUp}
+      >
+        <Text style={{ color: 'white', textAlign: 'center', fontSize: 16 }}>Criar conta</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#222',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
