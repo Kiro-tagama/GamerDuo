b599dbcdf763ccd07905e31e1aec20679cda2b22
Felipe Schmitz <104650985+devmitz@users.noreply.github.com>
2023-08-08 13:25:16 -0300

    Update Firebase.ts

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -5,15 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBxVgAoumRO8vTDHJJTp_n2-rl37Qmq29o",
-  authDomain: "gamerduo-251ce.firebaseapp.com",
-  projectId: "gamerduo-251ce",
-  storageBucket: "gamerduo-251ce.appspot.com",
-  messagingSenderId: "76517276234",
-  appId: "1:76517276234:web:92efd437895442233404b3",
-  measurementId: "G-N5JBQC71B1"
-};
+
 
 // Initialize Firebase
 export const Firebase = initializeApp(firebaseConfig);
